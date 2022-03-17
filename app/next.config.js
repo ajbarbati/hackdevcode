78c12bab60d5ef9b24b860d28c2d112fbe9fae92
Alex <ajbarbati@gmail.com>
2022-03-17 10:11:11 -0400

    first dev commit of forked nextjs blog

---
+++ app/next.config.js
@@ -0,0 +1,50 @@
+
+
+// next.config.js
+/*
+ * Let's reuse the client configuration. I have rewritten
+ * it as a CommonJS module just to make it runs in a Node
+ * environment without any fuzz.
+ */
+
+
+// const client = require('./client.js')
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      },
+    ]
+  },  images: {
+    loader: 'cloudinary',
+    path: '',
+  },
+
+  // exportPathMap: async function (defaultPathMap) {
+  //   const paths = await client
+  //     .fetch('*[_type == "post" && defined(slug)].slug.current')
+  //     .then(data =>
+  //       data.reduce(
+  //         (acc, slug) => ({
+  //           '/': { page: '/' },
+  //           ...acc,
+  //           [`/post/${slug}`]: { page: '/post/[slug]', query: { slug } }
+  //         }),
+  //         defaultPathMap
+  //       )
+  //     )
+  //     .catch(console.error)
+  //   return paths
+  // },
+  experimental: {css: true}
+}
+
+  
